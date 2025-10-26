8c264f7a984e21478986890c57987916e59efa5f
yu13140 <whmyc801@gmail.com>
2025-10-27 00:55:30 +0800

    Revert "@fc context"
    - To avoid adding additional functions, restore the added @fc context.
    - refer to https://github.com/Star-Seven/susfs4ksu/blob/M62-backport-Susfs/kernel_patches/50_add_susfs_in_kernel-4.14.patch
    
    Change-Id: I24b9d8e6d907513cc537ab0eba0003192f818a9d
    Signed-off-by: yu13140 <whmyc801@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1136,13 +1136,13 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 		return ERR_PTR(-ENODEV);
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	// We keep checking for ksu process
-	if (susfs_is_current_ksu_domain()) {
-		mnt = susfs_alloc_sus_vfsmnt(fc->source ?: "none");
-		goto bypass_orig_flow;
-	}
+    // We only check for ksu process
+        if (susfs_is_current_ksu_domain()) {
+                mnt = susfs_alloc_sus_vfsmnt(name);
+                goto bypass_orig_flow;
+        }
 #endif
-	mnt = alloc_vfsmnt(fc->source ?: "none");
+         mnt = alloc_vfsmnt(name);
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 bypass_orig_flow:
 #endif
